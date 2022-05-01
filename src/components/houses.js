c8345b1aec6c1c6a488cc5268fe5b0dfb658713c
Yernar Yergaziyev <ernarergaziev00@gmail.com>
2022-05-02 05:19:29 +0600

    flats

--- src/components/houses.js
+++ src/components/houses.js
@@ -110,30 +110,37 @@ class HousePage extends React.Component {
 															<path fillRule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
 															<path fillRule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
 														</svg>
-														Тип недвижимости: 
+														Тип недвижимости: Квартира
 													</p>
 													<p className="house-card-district">
 														<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house" viewBox="0 0 16 16">
 															<path fillRule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
 															<path fillRule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
 														</svg>
-														Комнатность: 
+														Комнатность: {house.room} + 1
 													</p>
 													<p className="house-card-district">
 														<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrows-fullscreen" viewBox="0 0 16 16">
 															<path fillRule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707zm0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707zm-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707z"/>
 														</svg>
-														Площадь: 
+														Площадь: {house.area ? house.area : `224`}
 													</p>
 													<NumberFormat
-														value={house.priceTenge}
+														value={house.priceEuro}
 														displayType={'text'}
 														thousandSeparator={` `}
 														suffix={`€`}
 														renderText={(value, props) => <p className="house-card-price euro-price" {...props}>{value}</p>}
 													/>
 													<NumberFormat
-														value={house.priceEuro}
+														value={house.priceDollar ? house.priceDollar : house.priceEuro}
+														displayType={'text'}
+														thousandSeparator={` `}
+														suffix={`$`}
+														renderText={(value, props) => <p className="house-card-price dollar-price" {...props}>{value}</p>}
+													/>
+													<NumberFormat
+														value={house.priceTenge}
 														displayType={'text'}
 														thousandSeparator={` `}
 														suffix={`₸`}
